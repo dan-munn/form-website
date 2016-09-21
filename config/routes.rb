fc2a4748b3f9b5e890968f1c4c3271e109799bcf
daniel munn <danielmunn@outlook.com>
2016-09-21 17:08:06 +0000

    Contact database. model & migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'about' => 'pages#about'
+  get 'about'     => 'pages#about'
+  
+  get 'home'      => 'pages#home'
+  
+  get 'homeowner' => 'pages#homeowner'
 
   root 'pages#home'
 
